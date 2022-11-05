3ca5d490f78160dc91608d853ff4cca827ab7897
Dominik <domin852321@gmail.com>
2022-11-05 13:07:08 +0100

    add admin lte

--- projects/mfe-profile/src/app/profile/profile.component.ts
+++ projects/mfe-profile/src/app/profile/profile.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { OktaAuthStateService } from '@okta/okta-angular';
+import { Component } from '@angular/core';
 import { filter, map } from 'rxjs';
 
 @Component({
@@ -8,16 +7,7 @@ import { filter, map } from 'rxjs';
   styles: []
 })
 export class ProfileComponent {
-  public profile$ = this.oktaStateService.authState$.pipe(
-      filter(state => !!state && !!state.isAuthenticated),
-      map(state => state.idToken?.claims)
-  );
 
-  public date$ = this.oktaStateService.authState$.pipe(
-      filter(state => !!state && !!state.isAuthenticated),
-      map(state => (state.idToken?.claims.auth_time as number) * 1000),
-      map(epochTime => new Date(epochTime)),
-  );
 
-  constructor(private oktaStateService: OktaAuthStateService) { }
+  constructor() { }
 }
